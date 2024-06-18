4de7fe12b0e78891e0d66b3f7fdeebe8bcd94fc0
Ivan Junior <ivan_junior_we@hotmail.com>
2024-06-18 17:39:26 -0300

    Auto Scaling Group

---
+++ autoscaling-group.tf
@@ -0,0 +1,16 @@
+resource "aws_autoscaling_group" "autoscaling_ec2" {
+  name               = "autoscaling_ec2"
+  max_size           = 5
+  min_size           = 2
+  desired_capacity   = 3
+  availability_zones = ["us-east-1a"]
+  force_delete       = true
+
+  launch_template {
+    id      = aws_launch_template.launch_template.id
+    version = "$Latest"
+  }
+
+  depends_on = [aws_launch_template.launch_template]
+
+}
