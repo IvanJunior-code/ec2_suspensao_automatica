748d2cc4a3aa28a78ecbdaceaf1b3e40f19bcbba
Ivan Junior <ivan_junior_we@hotmail.com>
2024-06-20 18:11:30 -0300

    Outputs a serem exibidos após criar a infra

---
+++ output.tf
@@ -0,0 +1,9 @@
+output "connection" {
+  value = "ssh -i ~/.ssh/${aws_key_pair.key_pair.key_name} ubuntu@.compute-1.amazonaws.com"
+}
+
+output "instance_ips" {
+  value = join("\n", [for ip in data.aws_instances.ec2.public_ips : replace(ip, ".", "-")])
+  sensitive = false
+}
+
