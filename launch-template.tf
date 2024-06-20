0faa2d238dda52c15f2fb306e37b26b8fd3073dd
Ivan Junior <ivan_junior_we@hotmail.com>
2024-06-20 18:09:52 -0300

    Adicionado nome da chave para o template de EC2.

--- launch-template.tf
+++ launch-template.tf
@@ -2,4 +2,5 @@ resource "aws_launch_template" "launch_template" {
   name_prefix   = "launch_template"
   image_id      = "ami-0f9b57fe51681be3f"
   instance_type = "t3.small"
+  key_name      = aws_key_pair.key_pair.key_name
 }
