09d0d003ae9e2de70c15a6d87b3d7e00489bcf53
Ivan Junior <ivan_junior_we@hotmail.com>
2024-06-21 18:14:25 -0300

    Alterando para o agendamento ser feito um tempo após o horário atual do timestamp(), e alterado o crontab para horário desejado +3, pois o crontab trabalha com fuso horário +0 e no Brasil é -3, então, para ligar as instâncias as 9h foi preciso configurar no crontab as 12h.

--- autoscaling-schedule.tf
+++ autoscaling-schedule.tf
@@ -3,8 +3,8 @@ resource "aws_autoscaling_schedule" "turn_on_morning" {
   min_size               = 1
   max_size               = 4
   desired_capacity       = 4
-  start_time             = "2016-12-11T18:00:00Z"
-  recurrence             = "0 9 * * MON-FRI" # https://crontab.guru/
+  start_time             = timeadd(timestamp(), "10m")
+  recurrence             = "0 12 * * MON-FRI" # https://crontab.guru/ o cron trabalha com o horário +0
   autoscaling_group_name = aws_autoscaling_group.autoscaling_ec2.name
 }
 
@@ -13,7 +13,7 @@ resource "aws_autoscaling_schedule" "turn_off_evening" {
   min_size               = 0
   max_size               = 0
   desired_capacity       = 0
-  start_time             = "2016-12-11T18:00:00Z"
-  recurrence             = "0 18 * * MON-FRI"
+  start_time             = timeadd(timestamp(), "11m") # 1min de diferença do outro agendamento, por regra da AWS
+  recurrence             = "0 21 * * MON-FRI"
   autoscaling_group_name = aws_autoscaling_group.autoscaling_ec2.name
 }
